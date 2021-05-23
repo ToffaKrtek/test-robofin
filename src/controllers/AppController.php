7186e87f0b6d960f30d858b30ce4e79c5475ebd8
ToffaKrtek <krotov.anthon@yandex.ru>
2021-05-23 21:32:59 +0700

    Замена контента посредством Jquery+Ajax

--- src/controllers/AppController.php
+++ src/controllers/AppController.php
@@ -4,18 +4,46 @@ include_once(ROOT.'/components/Pagination.php');
     class AppController {
 
         public static function actionList() {
-            
+            $app = new App;
             $title = 'Список сотрудников';
+
             //пагинация
             $page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
             $perpage = 10;
             $offset = $perpage * ($page - 1);
+            $list_type = 'show_all';
 
-            $category = isset($_GET['category']) ? $_GET['category'] : 'all';
-            $total = App::count_user($category);
-            $staff = App::getAll($offset, $perpage);
-            $pagination = new Pagination($page, $perpage, $total[0]);
-           
+            if(isset($_GET['list_type'])) {
+                $list_type = isset($_GET['list_type']) ? $_GET['list_type'] : 'show_all';
+                switch ($list_type){
+                    case 'show_all':
+                        $total = $app->countStaff($list_type);
+                        $staff = $app->getAll($offset, $perpage, $list_type);
+                        $pagination = new Pagination($page, $perpage, $total[0]);
+                        // require_once(ROOT.'/views/index.php');  
+                        break;
+                    case 'show_fired':
+                        $total = $app->countStaff($list_type);
+                        $staff = $app->getFiredStaff($offset, $perpage, $list_type);
+                        $pagination = new Pagination($page, $perpage, $total[0]);
+                        break;
+                    case 'show_probation':
+                        $total = $app->countStaff($list_type);
+                        $staff = $app->getProbationStaff($offset, $perpage, $list_type);
+                        $pagination = new Pagination($page, $perpage, $total[0]);
+                        break;
+                    default:
+                        $total = $app->countStaff($list_type);
+                        $staff = $app->getAll($offset, $perpage, $list_type);
+                        $pagination = new Pagination($page, $perpage, $total[0]);
+                        break;
+                }
+            }
+            else {
+                $total = $app->countStaff($list_type);
+                $staff = $app->getAll($offset, $perpage, $list_type);
+                $pagination = new Pagination($page, $perpage, $total[0]);
+            }
             require_once(ROOT.'/views/index.php');  
             return true;
         }
--- src/models/App.php
+++ src/models/App.php
@@ -1,9 +1,26 @@
 <?php
     class App {
-        static function getAll($offset, $perpage){
+        // Статичные методы
+        // Общая выдача без каких-либо фильтров (но с пагинацией)
+        function getAll($offset, $perpage, $category){
+            $param = '';  // Весьма топорное решение, конечно. Но вроде сразу понятно, что происходит, т.ч. оставил
+            switch ($category){
+                case 'show_all':
+                    $param = '';
+                    break;
+                case 'show_fired':
+                    $param = 'WHERE ';
+                    break;
+                case 'show_probation':
+                    $param = '';
+                    break;
+                default:
+                    $param = '';
+                    break;
+            }
             try {
                 $db = DB::getConnect();
-                $sql = "SELECT * FROM user LIMIT $perpage OFFSET $offset";
+                $sql = "SELECT * FROM user $param ORDER BY last_name LIMIT $perpage OFFSET $offset";
                 $result = $db->query($sql);
             }  catch( Exception $e) {
                 print_r($e);
@@ -11,15 +28,89 @@
             }
             return $result->fetchAll();
         }
-        static function count_user($category){
+
+        // Получаем кол-во записей -- необходимо для корректной пагинации
+        function countStaff($category){
+            
+            $param = '';  
+            switch ($category){
+                case 'show_all':
+                    $param = '';
+                    break;
+                case 'show_fired':
+                    $param = ' INNER JOIN user_dismission ON user.id = user_dismission.user_id';
+                    break;
+                case 'show_probation':
+                    $param = ' WHERE created_at >= DATE_SUB(CURRENT_DATE, INTERVAL 3 MONTH)';
+                    break;
+                default:
+                    $param = '';
+                    break;
+            }
             try {
                 $db = DB::getConnect();
-                $sql = "SELECT COUNT(id) FROM user";
+                $sql = "SELECT COUNT(*) FROM user".$param;
                 $result = $db->query($sql);
             }  catch( Exception $e) {
                 print_r($e);
                 return $e;
             }
             return $result->fetch();
+        }
+
+        // Получаем список уволенных сотрудников
+        function getFiredStaff($offset, $perpage, $category) {
+            $param = '';  
+            switch ($category){
+                case 'show_all':
+                    $param = '';
+                    break;
+                case 'show_fired':
+                    $param = '';
+                    break;
+                case 'show_probation':
+                    $param = '';
+                    break;
+                default:
+                    $param = '';
+                    break;
+            }
+            try {
+                $db = DB::getConnect();
+                $sql = "SELECT * FROM user INNER JOIN user_dismission ON user.id = user_dismission.user_id $param ORDER BY last_name LIMIT $perpage OFFSET $offset";
+                $result = $db->query($sql);
+            }  catch( Exception $e) {
+                print_r($e);
+                return $e;
+            }
+            return $result->fetchAll();
+        }
+
+        // Получаем список сотрудников с испытательным сроком (3 месяца от текущего дня)
+        function getProbationStaff($offset, $perpage, $category) {
+            $param = '';  
+            switch ($category){
+                case 'show_all':
+                    $param = '';
+                    break;
+                case 'show_fired':
+                    $param = ' ';
+                    break;
+                case 'show_probation':
+                    $param = '';
+                    break;
+                default:
+                    $param = '';
+                    break;
+            }
+            try {
+                $db = DB::getConnect();
+                $sql = "SELECT * FROM user WHERE created_at >= DATE_SUB(CURRENT_DATE, INTERVAL 3 MONTH) $param ORDER BY last_name LIMIT $perpage OFFSET $offset";
+                $result = $db->query($sql);
+            }  catch( Exception $e) {
+                print_r($e);
+                return $e;
+            }
+            return $result->fetchAll();
         }
     }
